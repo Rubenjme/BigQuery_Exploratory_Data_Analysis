d0b9bec5bcce4a53f387cce4b4581e6f755ca667
Rubenjme <rubjaicab@gmail.com>
2025-01-30 17:08:24 +0100

    Se actualiza el rango de temperaturas a uno más restrictivo

--- SQL files/Outliers.sql
+++ SQL files/Outliers.sql
@@ -1,8 +1,9 @@
 -- Valores anómalos
 SELECT * FROM `Data_Queries.features` 
-WHERE Temperature < 15 OR Temperature > 40 
+WHERE Temperature < 14 OR Temperature > 95  --Grados Fahrenheit (Menor a -10ºC y mayor a 35ºC )
 ORDER BY Store;
 
 SELECT * FROM `Data_Queries.sales` 
 WHERE Weekly_Sales < 0 
 ORDER BY Weekly_Sales;
+

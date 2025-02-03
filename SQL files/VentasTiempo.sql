ce8c8079c1905aed12c3c63aca64e371e9bb746b
Rubenjme <rubjaicab@gmail.com>
2025-02-03 19:32:12 +0100

    Se añade la columna Store al archivo de ventas a lo largo del tiempo. Se añade el pdf tras usar Looker Studio.

--- SQL files/VentasTiempo.sql
+++ SQL files/VentasTiempo.sql
@@ -1,15 +1,18 @@
 -- Creo una vista de "Ventas a lo largo del tiempo"
 CREATE OR REPLACE VIEW `Data_Queries.sales_over_time` AS
 SELECT  
-    s.Date,  
+    s.Date,
+    s.Store,  
     Round(SUM(s.Weekly_Sales),2) AS Total_Sales,  
     Round(AVG(s.Weekly_Sales),2) AS Avg_Sales  
 FROM  
     `Data_Queries.sales` s  
 WHERE s.Weekly_Sales >= 0  
 GROUP BY  
-    s.Date  
+    s.Date,
+    s.Store  
 ORDER BY  
-    s.Date;  
+    s.Date,
+    s.Store;  
 
 select * from `Data_Queries.sales_over_time`;  -- Consulto la vista creada
